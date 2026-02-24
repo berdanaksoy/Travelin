508bd93c0d193df8e6153d05cde525d2bc9b74bb
Berdan Aksoy <berdan0227@gmail.com>
2026-02-24 16:03:05 +0300

    Created the CreateTour, TourList, and UpdateTour pages in the admin panel, made them functionally operational, and implemented the required logic in the admin controller.

--- Travelin/Controllers/AdminTourController.cs
+++ Travelin/Controllers/AdminTourController.cs
@@ -1,12 +1,59 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Travelin.Dtos.TourDtos;
+using Travelin.Services.TourServices;
 
 namespace Travelin.Controllers
 {
     public class AdminTourController : Controller
     {
-        public IActionResult TourList()
+        private readonly ITourService _tourService;
+
+        public AdminTourController(ITourService tourService)
+        {
+            _tourService = tourService;
+        }
+
+        public async Task<IActionResult> TourList()
+        {
+            var values = await _tourService.GetAllTourAsync();
+
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CreateTour()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTour(CreateTourDto createTourDto)
+        {
+            await _tourService.CreateTourAsync(createTourDto);
+
+            return RedirectToAction("TourList");
+        }
+
+        public async Task<IActionResult> DeleteTour(string id)
+        {
+            await _tourService.DeleteTourAsync(id);
+
+            return RedirectToAction("TourList");
+        }
+
+        public async Task<IActionResult> UpdateTour(string id)
+        {
+            var value = await _tourService.GetTourByIdAsync(id);
+
+            return View(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateTour(UpdateTourDto updateTourDto)
+        {
+            await _tourService.UpdateTourAsync(updateTourDto);
+
+            return RedirectToAction("TourList");
         }
     }
 }
